91d9ab50685acc49f48108c8fa7da66ce71e05b2
elad laufer <elad@wix.com>
2017-03-28 10:51:59 +0300

    - less log output

--- src/test/java/com/wix/mediaplatform/BaseTest.java
+++ src/test/java/com/wix/mediaplatform/BaseTest.java
@@ -20,9 +20,9 @@ public abstract class BaseTest {
 
     public BaseTest() {
 
-        System.setProperty("org.apache.commons.logging.Log","org.apache.commons.logging.impl.SimpleLog");
-        System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true");
-        System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.wire", "DEBUG");
+//        System.setProperty("org.apache.commons.logging.Log","org.apache.commons.logging.impl.SimpleLog");
+//        System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true");
+//        System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.wire", "DEBUG");
 
         try {
             this.httpClient = HttpClients.custom()
