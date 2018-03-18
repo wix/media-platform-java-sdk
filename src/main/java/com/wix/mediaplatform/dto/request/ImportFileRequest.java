771a276b81b9b1b8cd9a46fbf70a6c255eb3f478
dafnast <dafnast@wix.com>
2018-03-18 15:28:59 +0200

    added equals and hashCode to ImportFileRequest

--- src/main/java/com/wix/mediaplatform/dto/request/ImportFileRequest.java
+++ src/main/java/com/wix/mediaplatform/dto/request/ImportFileRequest.java
@@ -2,6 +2,8 @@ package com.wix.mediaplatform.dto.request;
 
 import com.wix.mediaplatform.dto.job.Destination;
 
+import java.util.Objects;
+
 public class ImportFileRequest {
 
     private String sourceUrl;
@@ -26,5 +28,20 @@ public class ImportFileRequest {
     public ImportFileRequest setDestination(Destination destination) {
         this.destination = destination;
         return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ImportFileRequest that = (ImportFileRequest) o;
+        return Objects.equals(sourceUrl, that.sourceUrl) &&
+                Objects.equals(externalAuthorization, that.externalAuthorization) &&
+                Objects.equals(destination, that.destination);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sourceUrl, externalAuthorization, destination);
     }
 }
