279588aa38d99a20378dcbb562a12ee428e627e4
elad laufer <elad@wix.com>
2020-03-30 15:06:52 +0300

    - fix field name

--- src/test/java/com/wix/mediaplatform/v6/service/image/ImageServiceTest.java
+++ src/test/java/com/wix/mediaplatform/v6/service/image/ImageServiceTest.java
@@ -28,6 +28,7 @@ public class ImageServiceTest extends BaseTest {
                 .execute();
 
         assertThat(imageFeatures.getFaces()[0].getX(), is(383));
+        assertThat(imageFeatures.getLabels()[0].getName(), is("cat"));
     }
 
     @Test
